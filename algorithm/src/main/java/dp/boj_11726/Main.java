e40179abc13afd3f0469023911c6f029f355cef8
Youngho <cyh6099@gmail.com>
2022-10-04 14:44:25 +0900

    백준 2xN타일 dp

---
+++ algorithm/src/main/java/dp/boj_11726/Main.java
@@ -0,0 +1,33 @@
+package dp.boj_11726;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new int[n + 1];
+
+        if (n >= 2) {
+            dp[1] = 1;
+            dp[2] = 2;
+        }
+        else{
+            System.out.println(1);
+            return;
+        }
+
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
