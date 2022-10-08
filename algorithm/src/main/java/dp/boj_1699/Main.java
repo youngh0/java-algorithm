8c8da75ac37ba85c8994d5962e96f25b1b0d05fe
Youngho <cyh6099@gmail.com>
2022-10-08 16:41:50 +0900

    백준 제곱수의 합 DP

---
+++ algorithm/src/main/java/dp/boj_1699/Main.java
@@ -0,0 +1,29 @@
+package dp.boj_1699;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = i;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - (j * j)] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+
+    }
+}
